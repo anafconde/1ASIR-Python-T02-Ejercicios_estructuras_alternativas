0cec12feb9604de1ffe37e21b0c87d61116fb618
Marco <mlinher0805@g.educaand.es>
2026-02-11 13:05:37 +0100

    Marco sube los ejercicios 1,3,5 y 7

---
+++ MarcoLinde/ej1.py
@@ -0,0 +1,9 @@
+#Ejercicio 1
+#Algoritmo que pida dos números e indique si el primero es 
+#mayor que el segundo o no.
+num1= float(input("Pon un número: "))
+num2= float(input("Pon otro número: "))
+if num1>num2:
+    print("El primer número es mayor que el segundo")
+else:
+    print("El primer número es menor o igual que el segundo")
---
+++ MarcoLinde/ej3.py
@@ -0,0 +1,7 @@
+#Ejercicio 3
+#Escribe un programa que lea un número e indique si es par o impar.
+num= int(input("Pon un número: "))
+if numero %2==0:
+    print("El número es par")
+else:
+    print("El número es impar")
