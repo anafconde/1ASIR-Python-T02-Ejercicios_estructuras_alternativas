8113efe3c3b1be2ef59d065eb2eedcde33bfaf11
miguel <cras9643@gmail.com>
2026-02-12 14:20:36 +0100

    Update 1-20 Pares

--- Miguel Ángel García Cortés/Ejercicio 10.py
+++ Miguel Ángel García Cortés/Ejercicio 10.py
@@ -7,3 +7,31 @@
 #tangentes interiores
 #interiores
 #concéntricas
+
+import math
+
+x1=int(input("Dime el valor de x1: "))
+x2=int(input("Dime el valor de x2: "))
+y1=int(input("Dime el valor de y1: "))
+y2=int(input("Dime el valor de y2: "))
+r1=int(input("Dime el valor de r1: "))
+r2=int(input("Dime el valor de r2: "))
+
+
+distancia = math.sqrt((x2-x1)**2+(y2-y1)**2)
+
+if distancia> r1+r2:
+    print("Son exteriores")
+elif distancia == r1+r2:
+    print("Son tangentes exteriores")
+elif r1-r2 < distancia < r1+r2:
+    print("Son secantes")
+elif distancia == r1-r2:
+    print("Son tangentes interiores")
+elif 0 < distancia < r1 - r2:
+    print("Son interiores")
+elif distancia == 0:
+    print("Son concentricas")
+else:
+    print("Error")
+print("Programa finalizado")
