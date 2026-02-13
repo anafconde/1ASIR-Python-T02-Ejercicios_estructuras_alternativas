f955da9b3c3a35d9512afa99e33b7a571f128e56
juliogomez <juliogomhoc@gmail.com>
2026-02-13 09:29:51 +0100

    corregidos

--- ejercicio2jgh.py
+++
@@ -1,9 +0,0 @@
-# Autor: Julio Gómez Hoces
-# Versión: 1.0
-# Ejercicio 2
-
-numero = int(input("Dime un número:"))
-if numero > 0:
-    print("El número es positivo")
-else :  
-    print("El número es negativo")
--- juliogomezhoces/ejercicio2jgh.py
+++ juliogomezhoces/ejercicio2jgh.py
@@ -2,3 +2,8 @@
 # Versión: 1.0
 # Ejercicio 2
 
+numero = int(input("Dime un número:"))
+if numero > 0:
+    print("El número es positivo")
+else :  
+    print("El número es negativo")
